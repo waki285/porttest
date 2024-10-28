3e442ce0d14d56a3a330d055efbc70c8e5d6d79f
すずねーう <suzuneu@suzuneu.com>
2024-10-28 20:02:35 +0900

    trace

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::net::SocketAddr;
-
 use axum::{extract::ConnectInfo, response::Html, routing::get, Router};
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let args: Vec<String> = std::env::args().collect();
     let port = args.get(1).unwrap_or(&"3000".to_string()).parse::<u16>().unwrap();
 
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        );
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
